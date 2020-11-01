959723a2173198ed83098d3cceef4ac68a957c44
Taguara Digital <taguaraDigital@hotmail.com>
2020-11-01 15:03:35 -0400

    validation

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,62 @@
-/* -----------------------------------------------------------------------------------------
-  Revisa en que link se hizo click, busca el link que tenia la clase active para removerla 
-  y luego agregarle al link donde se hizo click la clase active
------------------------------------------------------------------------------------------
-navItems.forEach(link => {
-   link.addEventListener('click', function(){
-      navList.querySelector('.nav__item--active').classList.remove('nav__item--active');
-      link.classList.add('nav__item--active');
-      
-      if (menuOpen) {
-         navLinks.classList.toggle('navbar--open');
-         mainNav.classList.toggle('nav-items--open');
-         toggleMenu.classList.toggle('toggle-menu--open');
-         menuOpen = false;
-      }
-   })
-});
--*/
+const form = document.getElementById('form');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+
+// validar correo
+const validateEmail = (email) => {
+   // const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+   const emailRegex = /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+   if(emailRegex.test(email)){return true;}
+   else{return false;}
+}
+
+
+
+const handleInput = (inputs) => {
+
+   resetErrors(inputs)
+   
+   if (fname.value ==='' || fname === null) {
+      showError(inputs.children[0].children)
+   }
+   
+   if (lname.value ==='' || lname === null) {
+      showError(inputs.children[1].children)
+   }
+
+   
+   if (email.value ==='' || !validateEmail(email.value)) {
+      showError(inputs.children[2].children)
+   }
+
+   if (password.value ==='' || password === null) {
+      showError(inputs.children[3].children)
+   }
+
+   
+}
+
+const showError = (element) => {
+   element[0].classList.add('error');
+   element[1].classList.remove('hidden');
+   element[2].classList.add('msg-error--show');
+}
+
+const resetErrors = (element) => {
+   for (let i = 0; i < 4; i += 1) {
+      element.children[i].children[0].classList.remove('error');
+      element.children[i].children[1].classList.add('hidden');
+      element.children[i].children[2].classList.remove('msg-error--show');
+   }
+}
+
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault()
+   handleInput (e.target)
+
+})
